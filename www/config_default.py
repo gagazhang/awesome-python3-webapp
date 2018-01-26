e6aa004144e644c267ba7d0bb9eccbe49edac84e
tjvalidteam <iflytek_tj_dev@163.com>
2018-01-26 15:11:18 +0800

    add blog list module

---
+++ www/config_default.py
@@ -0,0 +1,13 @@
+configs = {
+    'debug': True,
+    'db': {
+        'host': '10.4.67.111',
+        'port': 3306,
+        'user': 'root',
+        'password': 'root',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
